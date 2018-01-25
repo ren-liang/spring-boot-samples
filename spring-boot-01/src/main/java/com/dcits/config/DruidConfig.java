ed9688ac6f1d01fe8d01c7ad608b4186652b0fa4
renliangd <ren-liang@outlook.com>
2018-01-25 18:45:32 +0800

    #删除编译后文件

---
+++ spring-boot-01/src/main/java/com/dcits/config/DruidConfig.java
@@ -0,0 +1,35 @@
+package com.dcits.config;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DruidConfig {
+    @Bean
+    public ServletRegistrationBean druidServlet()
+    {
+        ServletRegistrationBean reg = new ServletRegistrationBean();
+        reg.setServlet(new StatViewServlet());
+        reg.addUrlMappings(new String[] { "/druid/*" });
+        reg.addInitParameter("loginUsername", "admin");
+        reg.addInitParameter("loginPassword", "fengchen123");
+        return reg;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean()
+    {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns(new String[] { "/*" });
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        filterRegistrationBean.addInitParameter("profileEnable", "true");
+        filterRegistrationBean.addInitParameter("principalCookieName", "USER_COOKIE");
+        filterRegistrationBean.addInitParameter("principalSessionName", "USER_SESSION");
+        return filterRegistrationBean;
+    }
+}
---
+++ spring-boot-01/src/main/java/com/dcits/web/UserController.java
@@ -0,0 +1,13 @@
+package com.dcits.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+    @RequestMapping(value={"/"}, method={org.springframework.web.bind.annotation.RequestMethod.GET}, produces={"application/json;charset=UTF-8"})
+    public String say()
+    {
+        return "index";
+    }
+}
